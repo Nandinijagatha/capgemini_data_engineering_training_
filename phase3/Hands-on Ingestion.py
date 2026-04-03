6c59d1ed5e8fb09e3e5c8b5181e57a91fa399dfb
Nandinijagatha <122232980+Nandinijagatha@users.noreply.github.com>
2026-04-03 13:55:49 +0530

    Update Hands-on Ingestion.py

--- phase3/Hands-on Ingestion.py
+++ phase3/Hands-on Ingestion.py
@@ -12,3 +12,33 @@ from pyspark.sql.functions import col
 df.filter(col("column").isNull()).show()
 df.filter(col("age").isNull() | col("name").isNull()).show()
 -------------------------------------------------------------
+#Clean data using dropna() 
+
+#Remove rows with ANY null
+df_clean = df.dropna()
+
+#Remove rows only if ALL values are null
+df_clean = df.dropna(how="all")
+
+#Remove based on specific columns
+df_clean = df.dropna(subset=["age", "name"])
+---------------------------------------------------------------
+#Replace NULL values
+
+#Fill all nulls with zero
+df_filled = df.fillna(0)
+
+#Fill specific column
+df_filled = df.fillna({"age": 0})
+------------------------------------------------------------------
+#Read JSON and Parquet sample files
+df_json = (
+    spark.read
+    .option("multiline", "true")
+    .json("/samples/file.json")
+)
+df_json.show()
+df_parquet = spark.read.parquet("/samples/file.parquet")
+df_parquet.show()
+df_parquet.printSchema()
+------------------------------------------------------------------
